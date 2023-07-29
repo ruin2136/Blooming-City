2ebd39a58bcac41439fbd69dee8b8f82127d8f40
ruin2136 <jjoon2136@naver.com>
2023-07-29 09:46:38 +0900

    Basic Setting
    - Basic setting
    - Basic Script

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int actionType;
+
+    private void Move()
+    {
+        
+    }
+
+    private void Action()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
