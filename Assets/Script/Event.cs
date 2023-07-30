8b4f545849347d9c8c8b441489371364f93e215c
ruin2136 <jjoon2136@naver.com>
2023-07-30 11:46:21 +0900

    피니쉬

--- Assets/Script/Event.cs
+++ Assets/Script/Event.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
