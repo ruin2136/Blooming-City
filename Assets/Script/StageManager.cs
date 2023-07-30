4ba429d0283b265e190212f8bf2bede3fb64f0bf
ruin2136 <jjoon2136@naver.com>
2023-07-30 09:43:20 +0900

    중간병합 3차
    중간병합 3차

--- Assets/Script/StageManager.cs
+++ Assets/Script/StageManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
     public GameObject[] plants;
     public bool stageClear=false;
     public int stageNumber;
+
+    public GameObject clearPopUp;
 
     // Update is called once per frame
     void Update()
@@ -22,10 +25,22 @@ public class StageManager : MonoBehaviour
             }
         }
 
-        if(stageClear)
+        CheckClear();
+    }
+
+    void CheckClear()
+    {
+        if (stageClear)
         {
             GameManager.instance.isClearStage[stageNumber] = true;
             //스테이지 클리어!
+
+            clearPopUp.SetActive(true);
         }
+    }
+
+    public void OnClickBtn()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
